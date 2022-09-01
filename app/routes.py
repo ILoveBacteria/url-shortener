c65ede2d581910d824868c159a342bb043effe65
Moein Arabi <moein.mo81@gmail.com>
2022-09-01 09:10:34 +0430

    Added login form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
 from app import app
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return render_template('index.html', title='Home')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash(f'Username: {form.username.data} - Password: {form.password.data}')
+        return redirect('/')
+
+    return render_template('login.html', title='Login', form=form)
+
