c8333af25908d281a6f87d272573f73167f5be5e
Moein Arabi <moein.mo81@gmail.com>
2022-09-03 16:13:54 +0430

    Create link table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -6,6 +7,19 @@ class User(db.Model):
     username = db.Column(db.String(255), index=True, unique=True)
     email = db.Column(db.String(255), index=True, unique=True)
     password_hash = db.Column(db.String(255))
+    links = db.relationship('Link', backref='user', lazy='dynamic')
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
         return f'User: {self.username}'
+
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(255), index=True, unique=True)
+    redirect_url = db.Column(db.String(4096))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'Link: {self.slug}'
