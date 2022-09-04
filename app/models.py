48c48848674583dc18ea4468777b67433443ff3f
Moein Arabi <moein.mo81@gmail.com>
2022-09-04 22:23:56 +0430

    Create sign up route and connect to database

--- app/models.py
+++ app/models.py
@@ -1,14 +1,23 @@
 from app import db
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+from app import login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), index=True, unique=True)
     email = db.Column(db.String(255), index=True, unique=True)
     password_hash = db.Column(db.String(255))
     links = db.relationship('Link', backref='user', lazy='dynamic')
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'User: {self.username}'
@@ -23,3 +32,8 @@ class Link(db.Model):
 
     def __repr__(self):
         return f'Link: {self.slug}'
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,52 @@
-from app import app
-from flask import render_template, flash, redirect
-from app.forms import LoginForm
+from app import app, db
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+from app.forms import LoginForm, SignUpForm
+from app.models import User
 
 
+@login_required
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html', title='Home')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
 
     if form.validate_on_submit():
-        flash(f'Username: {form.username.data} - Password: {form.password.data}')
-        return redirect('/')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Username or password is incorrect')
+            return redirect(url_for('login'))
+        login_user(user, remember=True)
+        return redirect(url_for('index'))
 
     return render_template('login.html', title='Login', form=form)
 
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = SignUpForm()
+
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)  # type: ignore
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Sign up successfully')
+        return redirect(url_for('login'))
+    return render_template('signup.html', title='Sign Up', form=form)
