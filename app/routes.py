24d8b7b72f3bfe58fa19ea035582c5c4e0a22a1b
Moein Arabi <moein.mo81@gmail.com>
2022-09-05 15:04:00 +0430

    Add create new link route

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,20 @@
-from app import app, db
-from flask import render_template, flash, redirect, url_for
+import random
+import string
+
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
-from app.forms import LoginForm, SignUpForm
-from app.models import User
+
+from app import app, db
+from app.forms import LoginForm, SignUpForm, NewLinkForm
+from app.models import User, Link
 
 
-@login_required
+def random_slug(size=6, chars=string.ascii_lowercase + string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html', title='Home')
 
@@ -24,7 +32,8 @@ def login():
             flash('Username or password is incorrect')
             return redirect(url_for('login'))
         login_user(user, remember=True)
-        return redirect(url_for('index'))
+        flash('Login successfully')
+        return redirect(request.args.get('next') or url_for('index'))
 
     return render_template('login.html', title='Login', form=form)
 
@@ -50,3 +59,25 @@ def signup():
         flash('Sign up successfully')
         return redirect(url_for('login'))
     return render_template('signup.html', title='Sign Up', form=form)
+
+
+@app.route('/links/new', methods=['GET', 'POST'])
+@login_required
+def create_new_link():
+    form = NewLinkForm()
+
+    if form.validate_on_submit():
+        link = Link(slug=random_slug(), redirect_url=form.redirect_url.data, user=current_user)
+        db.session.add(link)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('new_link.html', title='Create New Link', form=form)
+
+
+@app.route('/r/<slug>')
+def short_url(slug):
+    link = Link.query.filter_by(slug=slug).first()
+    if link is not None:
+        return redirect(link.redirect_url)
+    return '404', 404
