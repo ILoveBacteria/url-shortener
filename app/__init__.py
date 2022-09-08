456c41dfbe36f95fa6fa8cea606e0d6c56f03a6f
Moein Arabi <moein.mo81@gmail.com>
2022-09-08 10:37:57 +0430

    Add flask debug toolbar

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+toolbar = DebugToolbarExtension(app)
 
 # The user will be redirected if he was not logged in to the user account
 login.login_view = 'login'
