01614d215b043dd9b5cfdf7d41768cc02909ab12
Moein Arabi <moein.mo81@gmail.com>
2022-09-17 10:24:53 +0430

    Read Database url from second environ var

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ac%sA2S@dw!'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or os.environ.get('DATABASE_URL')
