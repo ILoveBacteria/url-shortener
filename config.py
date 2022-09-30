6efec330d4f8a15773a31dfa0fa248f09b7c8a05
Moein Arabi <moein.mo81@gmail.com>
2022-09-30 23:23:03 +0330

    Correct database uri

--- config.py
+++ config.py
@@ -4,4 +4,11 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ac%sA2S@dw!'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or os.environ.get('DATABASE_URL')
+
+    @classmethod
+    @property
+    def SQLALCHEMY_DATABASE_URI(cls):
+        link = os.environ.get('DATABASE_URL')
+        if not link.startswith('postgresql'):
+            link = link.replace('postgres', 'postgresql', 1)
+        return link
